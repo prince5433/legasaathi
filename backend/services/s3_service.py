c7a85b1248ea989b59d6edd53cd9cecfd45ae08e
Prince Seth <prince11mayminote7pro@gmail.com>
2026-06-19 20:07:06 +0530

    file khholen fixed on aws

--- backend/services/s3_service.py
+++ backend/services/s3_service.py
@@ -28,6 +28,7 @@ def _s3_client():
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
         region_name=settings.AWS_REGION,
+        endpoint_url=f"https://s3.{settings.AWS_REGION}.amazonaws.com",
     )
 
 
